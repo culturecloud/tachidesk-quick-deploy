b8facf1863b45c2dffcd622c0d4754320bd05b07
RedoX <pseudokawaii@gmail.com>
2023-02-17 06:44:14 +0000

    Remove possible duplicates after backup

--- backup.sh
+++ backup.sh
@@ -11,7 +11,10 @@ while [[ -f $XDG_CONFIG_HOME/rclone/rclone.conf ]]; do
   echo -e "\033[1;33mStarting backup task...\033[0m"
   rclone md5sum hlocal: >/dev/null
   rclone --progress sync hlocal: hbackup: --exclude="{*.tmp,local/**,webUI/**}"
+  echo -e "\033[1;33mDownloading local library...\033[0m"
   rclone --progress sync hbackup:local hlocal:local
-  echo -e "\033[1;33mBackup complete!\033[0m"
+  echo -e "\033[1;33mRemoving duplicates...\033[0m"
+  rclone --progress dedupe hbackup: --dedupe-mode="newest"
+  echo -e "\033[1;33mBackup task finished!\033[0m"
   sleep ${BACKUP_FREQUENCY:=3600}
 done
